ba584c5583994878bcce6fdf2117995b23129d3f
daya-prac <zonek163002@163.com>
2015-01-31 00:32:35 +0800

    The first CPP program

---
+++ myfirst.C
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won regret it!" << endl;
+    return 0;
+}
+
